e45864df981d947d8ce5498c60d51e1173e72dc7
Kishan <kdewasi@myseneca.ca>
2025-06-09 10:16:08 -0400

    Ignore fragments-backend for Jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// jest.config.js
+const path = require('path');
+const envFile = path.join(__dirname, 'env.jest');
+require('dotenv').config({ path: envFile });
+
+console.log(`Using LOG_LEVEL=${process.env.LOG_LEVEL}. Use 'debug' in env.jest for more detail`);
+
+module.exports = {
+  verbose: true,
+  testTimeout: 5000,
+  testPathIgnorePatterns: ['/node_modules/', '/fragments-backend/'],
+};
